3cd31f39a4f089d7b6faa005328ad4793861f840
しゃるしぃ <info@angelbell.jp>
2019-04-05 16:02:07 +0900

    Add a project.

---
+++ MillviVideoDownloader/MillviVideoDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MillviVideoDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
